244021a44cb7fbe4ccbbd5ca7047527b299c4df7
stefanzweifel <stefanzweifel@users.noreply.github.com>
2022-01-10 14:06:21 +0000

    Fix styling

--- src/Support/HasSendsTrait.php
+++ src/Support/HasSendsTrait.php
@@ -5,13 +5,12 @@ declare(strict_types=1);
 namespace Wnx\Sends\Support;
 
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
-use Wnx\Sends\Models\Send;
 
 trait HasSendsTrait
 {
     public function sends(): MorphToMany
     {
         return $this
-            ->morphToMany( config('sends.send_model'), 'sendable','sendables','sendable_id','send_id');
+            ->morphToMany(config('sends.send_model'), 'sendable', 'sendables', 'sendable_id', 'send_id');
     }
 }
